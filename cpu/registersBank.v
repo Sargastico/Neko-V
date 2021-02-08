14ad2d5e2f3553e5e280fed67fc9e78edb31c91d
sargastico <sargastico@github.com>
2021-02-08 15:58:59 -0800

    Fixed register array variable and check

--- cpu/registersBank.v
+++ cpu/registersBank.v
@@ -1,11 +1,20 @@
+/*
+
+The "zero register" or 'x0', cannot be modified and always stores the constant zero.
+Before assign any value coming from dataIn, the 'if statement' (Line 46) checks if the register 'x0' has not been selected.
+
+sauce: https://en.wikichip.org/wiki/risc-v/registers
+
+*/
+
 module registerBank (
 
-    input   clk,
-    input   reset, 
-    input   [31:0]dataIn,   // 32-bit register
-    output  [31:0]dataOut,  
-    input   [16:0]select,   // Select 1 of 16 Registers
-    input   write           // 0 = Disable | 1 = Enable 
+    input  wire  clk,
+    input  wire  reset, 
+    output wire  [31:0]dataOut,  // 32-bit register
+    input        [31:0]dataIn,   
+    input        [16:0]select,   // Select 1 of 16 Registers
+    input        write           // 0 = Disable | 1 = Enable 
 
 );
 
@@ -13,7 +22,6 @@ integer i;
 reg [31:0] registers [0:15]; //16 registers of 32 bits
 
 initial begin 
-
 
     for (i = 0; i < 16; i++) begin
         
@@ -32,20 +40,14 @@ always @(posedge clk ) begin
             registers[i] <= 0;
 
         end
-
+        
     end
 
-    /*
-
-    The "zero register" or 'x0', cannot be modified and always stores the constant zero.
-    Before assign any value coming from dataIn, the 'if statement' checks if the register 'x0' has not been selected.
-
-    sauce: https://en.wikichip.org/wiki/risc-v/registers
-
-    */
-    else if (write && (select != 0))
+    else if ((write == 1) && (select != 0)) begin
 
         registerBank[select] <= dataIn;
+
+    end
     
 end
 

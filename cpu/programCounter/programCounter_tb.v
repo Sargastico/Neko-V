f068e3e10f1bc983ebe6085df8a779e13210cecc
sargastico <sargastico@github.com>
2021-02-17 17:23:12 -0800

    fixed clock on unit test of Program Counter

--- cpu/programCounter/programCounter_tb.v
+++ cpu/programCounter/programCounter_tb.v
@@ -9,15 +9,13 @@ module programCounterTest;
     reg           reset = 0;
     reg   [31:0]  dataIn;
     wire  [31:0]  dataOut;
-    reg           writeEnable;     // 1 => WRITE, 0 => READ
-    reg           countEnable;     // 1 => COUNT UP, 0 => STOPPED
-    reg           writeAdd;        // 1 => Add to PC, 0 => Set to PC
-
-    always #10 clk = ~clk;
+    reg           write;     // 1 => WRITE, 0 => READ
+    reg           count;     // 1 => COUNT UP, 0 => STOPPED
+    reg           writeAdd;  // 1 => Add to PC, 0 => Set to PC
 
     programCounter counter(.clk(clk), .reset(reset), .dataIn(dataIn), 
-                            .dataOut(dataOut), .writeEnable(writeEnable),
-                             .writeAdd(writeAdd), .countEnable(countEnable));
+                            .dataOut(dataOut), .write(write),
+                             .writeAdd(writeAdd), .count(count));
 
     initial begin
 
@@ -26,93 +24,112 @@ module programCounterTest;
         $display("[!] Testing Program Counter");
         
         // Set Reset conditions
+        clk = 0;
         reset = 1;
         dataIn = 0;
-        writeEnable = 0;
-        countEnable = 0;
+        write = 0;
+        count = 0;
         writeAdd = 0;
 
         #10
+        clk = 1;
+        #10
+        clk = 0;
 
         // Test reset
         if (dataOut != 0) begin
             
-            $error("Expected dataOut to be [%d] but got [%d].", 0, dataOut);
+            $error("[-] Test Reset: Expected dataOut to be [%d] but got [%d].", 0, dataOut);
 
         end
 
         if (counter.programCounter != 0) begin 
             
-            $error("Expected counter.programCounter to be [%d] but got [%d].", 0, counter.programCounter);
+            $error("[-] Test Reset: Expected counter.programCounter to be [%d] but got [%d].", 0, counter.programCounter);
 
         end
 
         // Test Jump (Write-Set)
         dataIn = 32'hDEADBEEF;
-        writeEnable = 1;
+        write = 1;
         reset = 0;
 
         #10
+        clk = 1;
+        #10
+        clk = 0;
 
         dataIn = 0;
 
         if (dataOut != 32'hDEADBEEF) begin
 
-            $error("Expected dataOut to be [%d] but got [%d].", 32'hDEADBEEF, dataOut);
+            $error("[-] Test Jump Write-Set: Expected dataOut to be [%d] but got [%d].", 32'hDEADBEEF, dataOut);
             
         end
 
         if (counter.programCounter != 32'hDEADBEEF) begin 
            
-            $error("Expected counter.programCounter to be [%d] but got [%d].", 32'hDEADBEEF, counter.programCounter);
+            $error("[-] Test Jump Write-Set: Expected counter.programCounter to be [%d] but got [%d].", 32'hDEADBEEF, counter.programCounter);
         
         end
 
         // Test Jump (Write-Add)
         counter.programCounter = 32'hC;
         dataIn = 32'hFFFFFFFC; // -4
-        writeEnable = 1;
+        write = 1;
         writeAdd = 1;
         reset = 0;
 
         #10
+        clk = 1;
+        #10
+        clk = 0;
 
         dataIn = 0;
 
         if (dataOut != 32'h4) begin
             
-            $error("Expected dataOut to be [%d] but got [%d].", 32'h4, dataOut);
+            $error("[-] Test Jump Write-Add: Expected dataOut to be [%d] but got [%d].", 32'h4, dataOut);
 
         end 
 
         if (counter.programCounter != 32'h4) begin
             
-            $error("Expected counter.programCounter to be [%d] but got [%d].", 32'h4, counter.programCounter);
+            $error("[-] Test Jump Write-Add: Expected counter.programCounter to be [%d] but got [%d].", 32'h4, counter.programCounter);
 
         end 
 
         // Test Counter
         dataIn = 32'h00000004;
-        writeEnable = 1;
+        write = 1;
         writeAdd = 0;
 
         #10
+        clk = 1;
+        #10
+        clk = 0;
 
-        writeEnable = 0;
-        countEnable = 1;
+        write = 0;
+        count = 1;
 
         for (i = 0; i < 16; i++)begin
+            
 
-            #10
             if (dataOut != dataIn + i * 4) begin
                 
-                $error("Expected dataOut to be [%d] but got [%d].", dataIn + i * 4, dataOut);
+                $error("[-] Test Counter: Expected dataOut to be [%d] but got [%d].", dataIn + i * 4, dataOut);
 
             end 
 
+            #10
+            clk = 1;
+            #10
+            clk = 0;    
+        
+
         end
 
-        #10
+        #100
 
         $finish;
 
